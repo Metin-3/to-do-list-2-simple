87b3375fe9732f23a568d686013eb52a64543ae8
Metin-3 <7kp0v0e@code.edu.az>
2024-10-26 01:15:21 +0400

    add ToDoList simple

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.trim() == "") {
+        alert("Kindly Enter Task Name!!!!")
+    }
+
+    else {
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+
+    document.querySelector("#newtask input").value = ""
+}
